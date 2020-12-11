7c24272853010b4903d4a749f69658a612de9a59
Larry <abc151087@gmail.com>
2020-12-11 10:57:23 +0800

    Update Form1.cs

--- Software/Software/Form1.cs
+++ Software/Software/Form1.cs
@@ -16,5 +16,32 @@ namespace Software
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: 這行程式碼會將資料載入 'hW4DataSet.Users' 資料表。您可以視需要進行移動或移除。
+            this.usersTableAdapter.Fill(this.hW4DataSet.Users);
+            // TODO: 這行程式碼會將資料載入 'hW4DataSet.Categories' 資料表。您可以視需要進行移動或移除。
+            this.categoriesTableAdapter.Fill(this.hW4DataSet.Categories);
+
+        }
+
+        private void categoriesBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.categoriesBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.hW4DataSet);
+
+        }
+
+        private void categoriesBindingNavigator_RefreshItems(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
