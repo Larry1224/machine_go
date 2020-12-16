0c5f4bfa01c7d20eb9175e560a94fc9834303a21
5401J <kb2532276@gmail.com>
2020-12-16 10:36:44 +0800

    test2

--- 2_tier_desktop/2_tier_desktop/Form1.cs
+++ 2_tier_desktop/2_tier_desktop/Form1.cs
@@ -16,5 +16,20 @@ namespace _2_tier_desktop
         {
             InitializeComponent();
         }
+
+        private void categoriesBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.categoriesBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.machine_goDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: 這行程式碼會將資料載入 'machine_goDataSet.Categories' 資料表。您可以視需要進行移動或移除。
+            this.categoriesTableAdapter.Fill(this.machine_goDataSet.Categories);
+
+        }
     }
 }
